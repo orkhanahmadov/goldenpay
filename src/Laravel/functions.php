bd3592aac346590f1162958e2c85eba2a4fd76df
orkhanahmadov <ahmadov90@gmail.com>
2019-04-30 20:12:00 +0000

    Apply fixes from StyleCI

--- src/Laravel/functions.php
+++ src/Laravel/functions.php
@@ -2,12 +2,13 @@
 
 use Orkhanahmadov\Goldenpay\GoldenpayService;
 
-if (! function_exists('goldenpay')) {
+if (!function_exists('goldenpay')) {
     /**
      * Helper method for Laravel.
      *
      * @param string|null $authKey
      * @param string|null $merchantName
+     *
      * @return GoldenpayService
      */
     function goldenpay(string $authKey = null, string $merchantName = null)
